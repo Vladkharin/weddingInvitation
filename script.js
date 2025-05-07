6a35405271e0bede8bce47815555510abbf2dc8b
vlad <kooooenienko@gmail.com>
2025-05-07 22:32:44 +0500

    fix

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const params = Object.fromEntries(urlParams);
+  const names = document.querySelector("#names");
+
+  function formatNames(arr) {
+    if (arr.length === 0) return "";
+    if (arr.length === 1) return arr[0];
+
+    const last = arr.pop();
+    return arr.join(", ") + " и " + last;
+  }
+
+  names.textContent = formatNames(params.names.split(", "));
+});
